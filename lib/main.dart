551ae94f3b21d7a59ce57f675c912e91adf503e5
FlutLab.io <>
2021-05-11 05:28:59 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MiFotoApp());
+
+class MiFotoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "MI EJEMPLO",
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity, //finvisualdensity
+      ), //fin theme
+      home: PaginaInicio(),
+    ); //fin del returnmaterialapp
+  } //finbuildmifotoapp
+} //finclasefoto
+
+class PaginaInicio extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Bordes en la app de Castro"),
+        centerTitle: true,
+      ), //fin appbar
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+
+              Container(
+                height: 100,
+                width: 200,
+                decoration: BoxDecoration(
+                  color: Colors.green[100],
+                  borderRadius: BorderRadius.circular(25),
+                  border: Border.all(
+                    color: Colors.orange[200],
+                    width: 5,
+                  ),
+                ),
+                child: Center(
+                  child: Text(
+                    'Zayra Castro',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ), //fin child center
+              ), //fin de el container nombre
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                height: 150,
+                width: 150,
+                decoration: BoxDecoration(
+                  color: Colors.orange,
+                  image: DecorationImage(image: NetworkImage("https://raw.githubusercontent.com/zayra09castro/Mis_Imagenes/main/MiFotoApp.jpg"), alignment: Alignment.topCenter),
+                  border: Border(
+                    top: BorderSide(
+                      color: Colors.green[300],
+                      width: 5.0,
+                    ),
+                    bottom: BorderSide(
+                      color: Colors.green[300],
+                      width: 5.0,
+                    ),
+                  ),
+                ), //fin de el decoration box
+              ), //fin de el container foto
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                height: 100,
+                width: 200,
+                child: TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.green,
+                        width: 5.0,
+                      ),
+                    ),
+                    //! Change the Focused Border
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.orange,
+                        width: 5.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ), //fin de el container del texto del grupo
+            ], //fin del children
+          ), //fin de la columna child
+        ), //fin del bodycenter
+      ), //finsinglechild
+    ); //fin scaffold
+  } //fin build widget pagina de inicio
+} //fin clase pagina de inicio
